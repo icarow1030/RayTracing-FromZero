d2d3afda310b8c23841bb2a561adc556f34b02f9
Icaro Gabriel <icarogabi10@gmail.com>
2026-03-30 16:33:02 -0300

    adding new utilities header file

---
+++ rt_utils.h
@@ -0,0 +1,14 @@
+#ifndef RT_UTILS_H
+#define RT_UTILS_H
+
+#include "vec3.h"
+#include "ray.h"
+
+void solve_sphere_quadratic(const ray& r, double& a, double& h, double& c, const point3& center, double radius) {
+    vec3 oc = center - r.origin();
+    a = r.direction().length_squared();
+    h = dot(r.direction(), oc);
+    c = oc.length_squared() - radius*radius;
+}
+
+#endif //RT_UTILS_H
--- sphere.h
+++ sphere.h
@@ -3,16 +3,15 @@
 
 #include "hittable.h"
 #include "vec3.h"
+#include "rt_utils.h"
 
 class sphere : public hittable {
 public:
     sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius)) {}
 
     bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
-        vec3 oc = center - r.origin();
-        auto a = r.direction().length_squared();
-        auto h = dot(r.direction(), oc);
-        auto c = oc.length_squared() - radius*radius;
+        double a, h, c;
+        solve_sphere_quadratic(r, a, h, c, center, radius);
 
         auto discriminant = h*h - a*c;
         if(discriminant < 0) return false;
@@ -25,7 +24,8 @@ public:
         }
         rec.t = root;
         rec.p = r.at(rec.t);
-        rec.normal = (rec.p - center) / radius;
+        auto outward_normal = (rec.p - center) / radius;
+        rec.set_face_normal(r, outward_normal);
         return true;
     }
 private:
